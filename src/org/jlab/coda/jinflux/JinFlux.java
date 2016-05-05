7c9cb201d8a4ef8c27bc6c60d2efc1562019c02e
gurjyan <gurjyan@jlab.org>
2016-05-05 12:39:27 -0400

    cleaned up the API. This is considered to be a betta release of the JinFlux

--- src/org/jlab/coda/jinflux/JinFlux.java
+++ src/org/jlab/coda/jinflux/JinFlux.java
@@ -1,3 +1,25 @@
+/*
+ *   Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ *
+ *   IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ *   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ *   OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ *   BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ *   PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ *   MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *   This software was developed under the United States Government license.
+ *   For more information contact author at gurjyan@jlab.org
+ *   Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
+
 package org.jlab.coda.jinflux;
 
 import org.influxdb.InfluxDB;
@@ -413,7 +435,6 @@ public class JinFlux {
         return point;
     }
 
-
     /**
      * Writes the table into the database. The time is defined and
      * recorded for all the data points od the table. If the
@@ -504,6 +525,10 @@ public class JinFlux {
         return rl;
     }
 
+    /**
+     * Private method to print the query result on the console
+     * @param r query result
+     */
     private void printQueryResult(QueryResult r) {
         if (r.getResults() != null) {
             for (QueryResult.Result qr : r.getResults()) {
